bdd711ae1f7bc585b6396b5fa4af21cfdaefb52e
sonusainier <rohitsaini@lambdatest.com>
2021-07-04 22:59:43 +0530

    Initial Commit

---
+++ PokemonSwiftUI/PokemonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSwiftUIApp.swift
+//  PokemonSwiftUI
+//
+//  Created by Rohit Saini on 04/07/21.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
